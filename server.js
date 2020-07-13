515a6d6755f293865a05e74f99e2f27cea55252d
Angela Tseng <angelatseng@berkeley.edu>
2020-07-12 17:27:06 -0700

    add subpage links

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const profiles = require('./profiles.json')
+const pages = require('./pages.json')
 
 const app = express();
 app.set('view engine', 'pug');
-app.use('/public',express.static('public'))
+app.use('/public', express.static('public'))
 
 const server = app.listen(3000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
@@ -16,6 +17,19 @@ for (link of profiles.links) {
 app.get('/', (req, res) => {
 	res.render('index', {
 		title : 'Angela Tseng',
-		links : profiles.links
+		links : profiles.links,
+		subpages: pages.subpages
 	});
+});
+
+app.get('/aboutme', (req, res) => {
+	res.render('aboutme');
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects');
+});
+
+app.get('/blog', (req, res) => {
+	res.render('blog');
 });
